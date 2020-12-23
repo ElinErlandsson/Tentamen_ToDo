eb4bdfae04dd49207bf838e13bde504525c7ffaf
Elin Erlandsson <elin.erlandsson@utb.ecutbildning.se>
2020-12-23 15:24:06 +0100

    get data

--- js/app.js
+++ js/app.js
@@ -1,10 +1,45 @@
-import Ui from './userinterFace.js'
+import GetData from "./data.js"
 
-(function app() {
+class UserInputForm {
+    constructor() {
+        this.form = document.querySelector('#form');
+        this.listContainer = document.querySelector('#list-container');
+        this.todoInput = document.querySelector('#todo-input');
 
-    Ui.addTasks()
-    Ui.deleteBtn()
+        this.form.addEventListener('submit', this.addListItems.bind(this));
+        this.listContainer.addEventListener('click', this.deleteBtn.bind(this));
+    }
 
+    addListItems(e) {
+        e.preventDefault();
 
+        const inputValue = this.todoInput.value;
 
-}());
+        if (inputValue == '') {
+            alert('Enter a text!');
+        } else {
+            this.listContainer.innerHTML += `
+                <div class="input-group mt-1">
+                    <div class="input-group-text">
+                        <input class="form-check-input" type="checkbox" aria-label="Checkbox for following text input">
+                    </div>
+                    <input type="text" class="form-control" aria-label="Text input with checkbox" value="${inputValue}">
+                    <button class="delete-btn btn btn-outline-secondary" type="button" id="button-addon2">✖︎</button>
+                </div>
+                `
+            this.todoInput.value = '';
+        }
+    }
+
+    deleteBtn(e) {
+        if (e.target.classList.contains('delete-btn')) {
+            const inputItem = e.target.parentElement;
+            this.listContainer.removeChild(inputItem);
+        }
+    }
+
+}
+
+const ui = new UserInputForm();
+GetData()
+
--- js/data.js
+++ js/data.js
@@ -1,34 +1,33 @@
-const listContainer = document.querySelector('#list-container');
 
-fetch('data.json')
+class GetData {
 
-    .then((response) => {
-        return response.json()
-    }).then((data) => {
-        console.log(data.shower)
+    Api() {
 
-        listContainer.innerHTML = `
-        <div class="input-items input-group mt-1">
-            <input type="text" class="form-control" aria-label="Text input with radio button" value="${data.dryClean}">
-            <button class="delete-btn btn btn-outline-secondary" type="button" id="button-addon2">✖︎</button>
-        </div> 
+        listContainer = document.querySelector('#list-container');
 
-        <div class="input-items input-group mt-1">
-            <input type="text" class="form-control" aria-label="Text input with radio button" value="${data.vacuum}">
-            <button class="delete-btn btn btn-outline-secondary" type="button" id="button-addon2">✖︎</button>
-        </div> 
+        fetch('data.json')
+            .then((response) => {
+                return response.json()
+            }).then((data) => {
 
-        <div class="input-items input-group mt-1">
-            <input type="text" class="form-control" aria-label="Text input with radio button" value="${data.shopping}">
-            <button class="delete-btn btn btn-outline-secondary" type="button" id="button-addon2">✖︎</button>
-        </div> 
+                console.log(data)
+                for (let i = 0; i < data.tasks.length; i++) {
+                    this.listContainer.innerHTML += `
+                <div class="input-group mt-1">
+                    <div class="input-group-text">
+                        <input class="form-check-input" type="checkbox" aria-label="Checkbox for following text input">
+                    </div>
+                    <input type="text" class="form-control" aria-label="Text input with checkbox" value="${data.tasks[i]}">
+                    <button class="delete-btn btn btn-outline-secondary" type="button" id="button-addon2">✖︎</button>
+                </div>
+                `
+                }
 
-        <div class="input-items input-group mt-1">
-            <input type="text" class="form-control" aria-label="Text input with radio button" value="${data.shower}">
-            <button class="delete-btn btn btn-outline-secondary" type="button" id="button-addon2">✖︎</button>
-        </div> 
-    `
+            }).catch((error) => {
+                console.log(error)
+            })
 
-    }).catch((error) => {
-        console.log(error)
-    })
+    }
+}
+
+export default GetData;
